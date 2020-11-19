eb482ade52f2be441117b19b1e48184a543539ac
Andrian Stoykov <andrian.stoykov@kcl.ac.uk>
2020-11-19 18:35:40 +0000

    Fix rails project

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get 'demo/index'
+
+  get 'demo/hello'
+
+  get 'demo/contact'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
