9c57c2050117d60f63ef8f8333b7e22b9b767dbe
Andrian Stoykov <andrian.stoykov@kcl.ac.uk>
2020-12-12 20:14:34 +0000

    More integration tests

--- test/integration/payments_flow_test.rb
+++ test/integration/payments_flow_test.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 require 'test_helper'
 
 class PaymentsFlowTest < ActionDispatch::IntegrationTest
@@ -16,6 +17,22 @@ class PaymentsFlowTest < ActionDispatch::IntegrationTest
   end
 
   test 'unsuccessful payment due to IBAN' do
+    sign_in_as_user(@user, 'seg2020')
+    post '/payments', params: { user: { firstName: 'Test payment',
+                                        amount: '50.50',
+                                        country: 'BG',
+                                        iban: 'GB000000000' } }
 
+    assert_redirected_to '/payments'
+  end
+
+  test 'successful payment' do
+    sign_in_as_user(@user, 'seg2020')
+    post '/payments', params: { user: { firstName: 'Test payment',
+                                        amount: '50.50',
+                                        country: 'GB',
+                                        iban: 'GB000000000' } }
+
+    assert_redirected_to '/payments'
   end
 end
