8ddb04d2d7722c87ad60255d6c7ea8b1b6e05dcd
Julianne Diaz <julianne.diaz@kcl.ac.uk>
2020-12-13 14:27:43 +0000

    Added seeds

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -2,6 +2,7 @@ class TransactionsController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @transactions = Transaction.all
   end
 
   #def show
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Transaction.destroy_all
+User.destroy_all
+Account.destroy_all
+
+#User.create([{ firstName: "John", lastName: "Oakes", email: "johnoakes@mail.com", password: "Oakes" }])
+#Account.create([{ currency: 0, user_id: 1 }])
+#Transaction.create([{ payee: 'McDonalds', amount: 2.99, date: Date.new(2020, 12, 11), transaction_type: 'CARD_PAYMENT', account_id: 1 }])
+
+transaction_list = [
+  [ "McDonalds", 2.99, Date.new(2020, 12, 11), 'CARD_PAYMENT', 1],
+  [ "Starbucks", 5.79, Date.new(2020, 12, 11), 'CARD_PAYMENT', 1],
+  [ "Post Office", 30.35, Date.new(2020, 12, 11), 'CARD_PAYMENT', 1 ]
+]
+
+transaction_list.each do |payee, amount, date, transaction_type, account_id|
+  Transaction.create(payee: payee, amount: amount, date: date, transaction_type: transaction_type, account_id: account_id)
+end
