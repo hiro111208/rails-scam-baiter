88ff0113979336562850a67f59ef017e0bfff08b
Julianne Diaz <julianne.diaz@kcl.ac.uk>
2020-12-15 03:13:33 +0000

    Fixed transactions page

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -2,7 +2,8 @@ class TransactionsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @transactions = Transaction.all
+    @transactions = Transaction.order('created_at DESC')
+    @accounts = Account.all
   end
 
   #def show
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,28 +6,13 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-#Transaction.destroy_all
-#User.destroy_all
+User.destroy_all
+Account.destroy_all
+Transaction.destroy_all
+user = User.create(firstName: 'TestF', lastName: 'TestL', email: 'test@example.com', password: 'password', password_confirmation: 'password')
+account = user.accounts.create(currency: 0, balance: 127.88)
 
-#user = User.new(firstName: 'TestF', lastName: 'TestL', email: 'testing@mail.com', password: 'testing')
-#user.save
-
-#User.create([{ firstName: "John", lastName: "Oakes", email: "johnoakes@mail.com", password: "Oakes" }])
-#Account.create([{ currency: 0, user_id: 1 }])
-#Transaction.create([{ payee: 'McDonalds', amount: 2.99, date: Date.new(2020, 12, 11), transaction_type: 'CARD_PAYMENT', account_id: 1 }])
-
-#account = Account.new(currency: 0, user_id: 5)
-#account.save
-
-#transaction = Transaction.new(payee: 'McDonalds', amount: 2.99, date: Date.new(2020, 12, 11), transaction_type: 'CARD_PAYMENT', account_id: 5)
-#transaction.save
-
-transaction_list = [
-  [ "McDonalds", 2.99, Date.new(2020, 12, 11), 'CARD_PAYMENT', 1],
-  [ "Starbucks", 5.79, Date.new(2020, 12, 11), 'CARD_PAYMENT', 1],
-  [ "Post Office", 30.35, Date.new(2020, 12, 11), 'CARD_PAYMENT', 1 ]
-]
-
-transaction_list.each do |payee, amount, date, transaction_type, account_id|
-  Transaction.create(payee: payee, amount: amount, date: date, transaction_type: transaction_type, account_id: account_id)
-end
+account.transactions.create(payee: 'Starbucks', amount: 2.75, date: Date.new(2020, 12, 1), transaction_type: 0)
+account.transactions.create(payee: 'McDonalds', amount: 9.25, date: Date.new(2020, 12, 1), transaction_type: 0)
+account.transactions.create(payee: 'Wasabi', amount: 5.75, date: Date.new(2020, 12, 2), transaction_type: 0)
+account.transactions.create(payee: 'Shell', amount: 27.5, date: Date.new(2020, 12, 3), transaction_type: 0)
