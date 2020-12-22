3a3252614d5448aedb8dcd79fd2eddfcb058ad6d
Hiro Funatsuka <hiro.funatsuka@kcl.ac.uk>
2020-12-22 14:04:10 +0900

    Add a functionality of an extra security measure

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_11_080958) do
+ActiveRecord::Schema.define(version: 2020_12_20_185344) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,14 @@ ActiveRecord::Schema.define(version: 2020_12_11_080958) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
     t.decimal "balance", default: "0.0"
+  end
+
+  create_table "simple_captcha_data", id: :serial, force: :cascade do |t|
+    t.string "key", limit: 40
+    t.string "value", limit: 6
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["key"], name: "idx_key"
   end
 
   create_table "transactions", force: :cascade do |t|
